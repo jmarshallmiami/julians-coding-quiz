23082699474bb474661a34c0fe41df8d34f7e985
Jules <j.marshallmiami@yahoo.com>
2022-03-26 13:41:30 -0500

    HTML shell

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// array of {questions and answers}
+
+
+// add event listener to my start button
+
+// event cb starts the quiz
+
+//starting quiz starts timer
+
+// when we select a choice we check for true or false (if TRUE submit score if FALSE reduce time)
+
+// load next question
+
+//imput for your name to save your score to localStorage
+
+// STRETCH: replace old high scores with new high scores
+
