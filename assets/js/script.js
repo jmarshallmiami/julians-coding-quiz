daa0424d1034f26c28a0e04df39825faedf1cf66
Jules <j.marshallmiami@yahoo.com>
2022-05-27 21:30:49 -0500

    added array of question and answers and timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,73 @@
+
+var startButton = document.querySelector(".start-btn");
+var orderedListEl = document.querySelector(".question-choices");
+var timer;
+
 // array of {questions and answers}
+var questionArray = [
+    {
+        question: "What is Julian's favorite color?",
+        choices: ["red", "blue", "black", "yellow"],
+        answer: "red"
+    },
+    {
+        question: "What is Julian's favorite color?",
+        choices: ["red", "blue", "black", "yellow"],
+        answer: "red"
+    },
+    {
+        question: "What is Julian's favorite color?",
+        choices: ["red", "blue", "black", "yellow"],
+        answer: "red"
+    },
+    {
+        question: "What is Julian's favorite color?",
+        choices: ["red", "blue", "black", "yellow"],
+        answer: "red"
+    },
+    {
+        question: "What is Julian's favorite color?",
+        choices: ["red", "blue", "black", "yellow"],
+        answer: "red"
+    }
+];
+
+//timer function
+function startQuiz() {
+    timer = setInterval(scoreCalc, 1000);
+};
+
+//starting quiz starts timer
+var startingTime = 60;
+function scoreCalc() {
+    
+    startingTime--;
+    console.log(startingTime);
+    if (startingTime === 0) {
+        clearInterval(timer);
+    }
+};
+
+
+
+var questionNumber = 0
 
 
 // add event listener to my start button
+startButton.addEventListener("click", startQuiz);
 
 // event cb starts the quiz
 
-//starting quiz starts timer
 
 // when we select a choice we check for true or false (if TRUE submit score if FALSE reduce time)
-
+var evaluateAnswer = function (event) {
+    console.log(event.target);
+    //check if answer in
+};
 // load next question
 
 //imput for your name to save your score to localStorage
 
-// STRETCH: replace old high scores with new high scores
+// STRETCH GOAL: replace old high scores with new high scores
 
+orderedListEl.addEventListener("click", evaluateAnswer);
